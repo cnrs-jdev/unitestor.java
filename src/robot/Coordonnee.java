4ca0efba1dd2de21a2c8bd9292b3cb1bd9f0f88c
fabriceambert <fabrice.ambert@femto-st.fr>
2013-08-21 14:47:51 +0200

    First commit

---
+++ src/robot/Coordonnee.java
@@ -0,0 +1,11 @@
+package robot;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fabrice
+ * Date: 21/08/13
+ * Time: 12:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class Coordonnee {
+}
